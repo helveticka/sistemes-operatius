5bc75ebd09ad7c8aac3da754c01afae3669b1c08
Harpo <03fangos.guineu@icloud.com>
2024-11-14 17:36:49 +0100

    Depurar y comentar nivel 1

--- P2/Niveles/nivel1.c
+++ P2/Niveles/nivel1.c
@@ -1,8 +1,11 @@
 // Autores: Xavier Campos, Pedro Felix, Harpo Joan
 
 #include "nivel1.h"
-
+/**
+ * Método principal del archivo.
+ */
 int main() {
+    // Se leen líneas de comando y las ejecutamos
     while (1) {
         if (read_line(line)) {
             execute_line(line);
@@ -10,7 +13,6 @@ int main() {
     }
     return EXIT_SUCCESS;
 }
-
 /**
  Imprime el prompt y lee una linea de consola.
  *line: puntero donde se almacenará la linea leída.
@@ -19,7 +21,7 @@ int main() {
 char *read_line(char *line) {
     print_prompt();
     fflush(stdout);
-    memset(line,'/0',COMMAND_LINE_SIZE);
+    memset(line, '\0', COMMAND_LINE_SIZE);
     // Lee una línea desde stdin
     if(fgets(line,ARGS_SIZE,stdin) != NULL){
         // Sustituye el carácter '\n' por '\0'
@@ -31,55 +33,71 @@ char *read_line(char *line) {
         // Verifica si fue por Ctrl + D
         if(feof(stdin)){
 #if DEBUGN1
-            printf("\rAdiós\n");
+            fprintf(stderr, GRIS "\rAdiós\n" RESET);
 #endif
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
 
     }
     return line;
 }
-
+/**
+ Ejecuta una línea de comandos.
+ *line: puntero a la línea de comandos.
+ return: 0 si todo fue bien, -1 si hubo algún error.
+ */
 int execute_line(char *line) {
     char *args[ARGS_SIZE];
+    // Si hay argumentos, checkea si son internos
     if(parse_args(args, line) > 0){
         check_internal(args);
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
-
+/**
+ Parsea una línea de comandos.
+ *args: array de punteros a char donde se almacenarán los argumentos.
+ *line: línea de comandos a parsear.
+ return: número de argumentos leídos.
+ */
 int parse_args(char **args, char *line) {
     const char *delimiters = " \t\n\r";
     char *token;
     unsigned int counter = 0;
     token = strtok(line,delimiters);
+    // Mientras haya tokens, se añaden al array de argumentos
     while(token != NULL){
         if(token[0] == '#'){
 #if DEBUGN1
-            printf(GRIS "[parse_args()→ token %d: %s]\n" RESET, counter,token);
+            fprintf(stderr, GRIS "[parse_args()→ token %d: %s]\n" RESET, counter,token);
 #endif
             token = NULL;
             args[counter] = token;
 #if DEBUGN1
-            printf(GRIS "[parse_args()→ token %d corregido: %s]\n" RESET, counter,token);
+            fprintf(stderr, GRIS "[parse_args()→ token %d corregido: %s]\n" RESET, counter,token);
 #endif
             counter++;
             return counter;
         }
         args[counter] = token;
 #if DEBUGN1
-        printf(GRIS "[parse_args()→ token %d: %s]\n" RESET, counter,token);
+        fprintf(stderr, GRIS "[parse_args()→ token %d: %s]\n" RESET, counter,token);
 #endif
         counter++;
         token = strtok(NULL,delimiters);
     }
+    // Se añade NULL al final del array de argumentos
     args[counter] = NULL;
 #if DEBUGN1
-    printf(GRIS "[parse_args()→ token %d: %s]\n" RESET, counter, token);
+    fprintf(stderr, GRIS "[parse_args()→ token %d: %s]\n" RESET, counter, token);
 #endif
     return counter;
 }
-
+/**
+ Checkea si el comando es interno.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 1 si es interno, 0 si no lo es.
+ */
 int check_internal(char **args) {
     int command = 0;
 
@@ -87,85 +105,113 @@ int check_internal(char **args) {
         return 0;
     }
 
-    if (!strcmp(args[0], "cd")) {
+    else if (!strcmp(args[0], "cd")) {
         internal_cd(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "export")) {
+    else if (!strcmp(args[0], "export")) {
         internal_export(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "source")) {
+    else if (!strcmp(args[0], "source")) {
         internal_source(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "jobs")) {
+    else if (!strcmp(args[0], "jobs")) {
         internal_jobs(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "fg")) {
+    else if (!strcmp(args[0], "fg")) {
         internal_fg(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "bg")) {
+    else if (!strcmp(args[0], "bg")) {
         internal_bg(args);
         command = 1;
     }
 
-    if (!strcmp(args[0], "exit")) {
+    else if (!strcmp(args[0], "exit")) {
         printf("Bye Bye\n");
         exit(0);
         return EXIT_SUCCESS;
     }
+
     return command;
 }
-
+/**
+ Cambia de directorio.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_cd(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_cd()→ Esta función cambiará de directorio]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 }
+/**
+ Asigna valores a variables de entorno.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_export(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_export()→ Esta función asignará valores a variables cd de entorno]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 }
-
+/**
+ Ejecuta un fichero de líneas de comandos.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_source(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_source()→ Esta función ejecutará un fichero de líneas de comandos]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 } 
-
+/**
+ Muestra los procesos que no están en foreground.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_jobs(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_jobs()→ Esta función mostrará el PID de los procesos que no estén en foreground]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 }
-
+/**
+ Trae los procesos más recientes al primer plano.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_fg(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_fg()→ Esta función traerá los procesos más recientes al primer plano]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 }
-
+/**
+ Muestra los procesos parados o en segundo plano.
+ *args: array de punteros a char con los argumentos introducidos.
+ return: 0 si todo fue bien
+ */
 int internal_bg(char **args) {
 #if DEBUGN1
     fprintf(stderr, GRIS "[internal_bg()→ Esta función mostrará los procesos parados o en segundo plano]\n" RESET);
 #endif
     return EXIT_SUCCESS;
 }
-
+/**
+ Imprime el prompt.
+ */
 void print_prompt(void) {
     user = getenv("USER");
     char path[200];
