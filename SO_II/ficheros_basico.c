aa515fff879c0d33420cc771b354dc32aa39b194
PedroFelix8 <pedritofelixj@gmail.com>
2025-02-20 11:12:37 +0100

    tamMB

--- SO_II/ficheros_basico.c
+++ SO_II/ficheros_basico.c
@@ -1,3 +1,11 @@
 // Autores: Xavier Campos, Pedro Félix, Harpo Joan
 
 #include "ficheros_basico.h"
+
+int tamMB(unsigned int nbloques){
+    int tamMB = (nbloques/8)/BLOCKSIZE;
+    if(nbloques%8 != 0){
+        tamMB++;
+    }
+    return tamMB;
+}
