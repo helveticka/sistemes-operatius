041129a6bc56dd38e24b52481134db834f71ca7b
PedroFelix8 <pedritofelixj@gmail.com>
2025-03-18 09:38:21 +0100

    permitir.c

---
+++ SO_II/permitir.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ficheros.h"
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        fprintf(stderr, "Sintaxis: permitir <nombre_dispositivo> <ninodo> <permisos>\n");
+        return FALLO;
+    }
+
+    // Obtener los argumentos
+    char *nombre_dispositivo = argv[1];
+    unsigned int ninodo = atoi(argv[2]);
+    unsigned char permisos = atoi(argv[3]);
+
+    // Montar el dispositivo
+    if (bmount(nombre_dispositivo) == -1) {
+        return FALLO;
+    }
+
+    // Cambiar permisos
+    if (mi_chmod_f(ninodo, permisos) == -1) {
+        bumount();
+        return FALLO;
+    }
+
+    // Desmontar el dispositivo
+    if (bumount() == -1) {
+        return FALLO;
+    }
+
+    return EXITO;
+}
