e49d904afa7a3f41e2be52413f1a6f65e323592d
XaviCampos2005 <xavicampos2005@gmail.com>
2025-03-24 19:15:11 +0100

    escribir.c

--- SO_II/escribir.c
+++ SO_II/escribir.c
@@ -1,93 +1,67 @@
 #include "ficheros.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
 
-#define BLOCKSIZE 1024
+#define NUM_OFFSETS 5
 
-int main(int argc, char **argv) {
-    if (argc < 4) {
-        fprintf(stderr, "Sintaxis: %s <nombre_dispositivo> <texto> <diferentes_inodos>\n", argv[0]);
-        return -1;
-    }
-    
-    // Montar el dispositivo virtual
-    if (bmount(argv[1]) == -1) {
-        fprintf(stderr, "Error al montar el dispositivo\n");
-        return -1;
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Sintaxis: ./escribir <nombre_dispositivo> <texto> <diferentes_inodos>\n");
+        return FALLO;
     }
 
+    char *nombre_dispositivo = argv[1];
     char *texto = argv[2];
-    unsigned int diferentes_inodos = atoi(argv[3]);
-    unsigned int offsets[] = {9000, 209000, 30725000, 409605000, 480000000};
-    int num_offsets = sizeof(offsets) / sizeof(offsets[0]);
+    int diferentes_inodos = atoi(argv[3]);
+    unsigned int offsets[NUM_OFFSETS] = {9000, 209000, 30725000, 409605000, 480000000};
 
     unsigned int ninodo;
-    if (!diferentes_inodos) {
-        ninodo = reservar_inodo('f', 6);
-        if (ninodo == -1) {
-            fprintf(stderr, "Error al reservar inodo\n");
-            bumount();
-            return -1;
-        }
-        printf("Inodo reservado: %u\n", ninodo);
+    int bytes_escritos, total_bytes = strlen(texto);
+
+    printf("longitud texto: %d\n", total_bytes);
+
+    // Montar el sistema de archivos
+    if (bmount(nombre_dispositivo) == FALLO) {
+        perror("Error montando el dispositivo");
+        return FALLO;
     }
 
-    for (int i = 0; i < num_offsets; i++) {
-        if (diferentes_inodos) {
+    for (int i = 0; i < NUM_OFFSETS; i++) {
+        if (diferentes_inodos || i == 0) {
             ninodo = reservar_inodo('f', 6);
-            if (ninodo == -1) {
-                fprintf(stderr, "Error al reservar inodo\n");
-                bumount();
-                return -1;
-            }
-            printf("Inodo reservado: %u\n", ninodo);
+            printf("\nNº inodo reservado: %d\n", ninodo);
         }
 
-        unsigned int offset = offsets[i];
-        unsigned int nbytes = strlen(texto);
-        char buf_original[nbytes];
-        memset(buf_original, 0, nbytes);
-        strcpy(buf_original, texto);
+        printf("offset: %u\n", offsets[i]);
 
         // Escribir en el inodo
-        int bytes_escritos = mi_write_f(ninodo, buf_original, offset, nbytes);
+        bytes_escritos = mi_write_f(ninodo, texto, offsets[i], total_bytes);
         if (bytes_escritos < 0) {
-            fprintf(stderr, "Error al escribir en el inodo %u\n", ninodo);
+            fprintf(stderr, "Error en la escritura\n");
             bumount();
-            return -1;
+            return FALLO;
         }
-        printf("Bytes escritos en el inodo %u: %d\n", ninodo, bytes_escritos);
-
-        // Leer para verificar
-        char buf_lectura[nbytes];
-        memset(buf_lectura, 0, nbytes);
-        int bytes_leidos = mi_read_f(ninodo, buf_lectura, offset, nbytes);
-        if (bytes_leidos < 0) {
-            fprintf(stderr, "Error al leer del inodo %u\n", ninodo);
-            bumount();
-            return -1;
-        }
-        write(1, buf_lectura, bytes_leidos);
-        printf("\n");
+        printf("Bytes escritos: %d\n", bytes_escritos);
 
         // Obtener información del inodo
         struct STAT stat;
-        if (mi_stat_f(ninodo, &stat) == -1) {
-            fprintf(stderr, "Error al obtener stat del inodo %u\n", ninodo);
+        if (mi_stat_f(ninodo, &stat) < 0) {
+            fprintf(stderr, "Error obteniendo stat\n");
             bumount();
-            return -1;
+            return FALLO;
         }
-        printf("Tamaño lógico: %u bytes, Bloques ocupados: %u\n", stat.tamEnBytesLog, stat.numBloquesOcupados);
+
+        printf("stat.tamEnBytesLog=%d\n", stat.tamEnBytesLog);
+        printf("stat.numBloquesOcupados=%d\n", stat.numBloquesOcupados);
     }
 
-    // Desmontar el dispositivo
-    if (bumount() == -1) {
-        fprintf(stderr, "Error al desmontar el dispositivo\n");
-        return -1;
+    // Desmontar el sistema de archivos
+    if (bumount() == FALLO) {
+        perror("Error desmontando el dispositivo");
+        return FALLO;
     }
 
-    return 0;
+    return EXITO;
 }
