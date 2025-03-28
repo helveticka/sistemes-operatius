1bb04596a52a90cc951ed929648c3cba6dbbb9af
helveticka <03fangos.guineu@icloud.com>
2025-03-28 11:25:58 +0000

    Comments bloques.c, bloques.h y leer_sf.c

--- SO_II/bloques.h
+++ SO_II/bloques.h
@@ -1,5 +1,7 @@
-// Autores: Xavier Campos, Pedro Félix, Harpo Joan
-
+/**
+ * @file bloques.h
+ * @author Xavier Campos, Pedro Félix, Harpo Joan
+ */
 #include <stdio.h>  //printf(), fprintf(), stderr, stdout, stdin
 #include <fcntl.h> //O_WRONLY, O_CREAT, O_TRUNC
 #include <sys/stat.h> //S_IRUSR, S_IWUSR
@@ -7,13 +9,11 @@
 #include <unistd.h> // SEEK_SET, read(), write(), open(), close(), lseek()
 #include <errno.h>  //errno
 #include <string.h> // strerror()
-
 // Constantes
 #define BLOCKSIZE 1024 // bytes
-
 #define EXITO 0 //para gestión errores
 #define FALLO -1 //para gestión errores
-
+// Códigos de colores
 #define BLACK   "\x1B[30m"
 #define RED     "\x1b[31m"
 #define GREEN   "\x1b[32m"
@@ -28,26 +28,24 @@
 #define LGREEN  "\x1B[38;2;17;245;120m"
 #define GRAY    "\x1B[38;2;176;174;174m"
 #define RESET   "\x1b[0m"
-
+// Mensajes de depuración
 #define NEGRITA "\x1b[1m"
-#define DEBUGN1 0
-#define DEBUGN2 0
-#define DEBUGN3 0
-#define DEBUGN4 0
-#define DEBUGN5 0
-#define DEBUGN6 1
-#define DEBUGN7 0
-#define DEBUGN8 0
-#define DEBUGN9 0
-#define DEBUGN10 0
-#define DEBUGN11 0
-#define DEBUGN12 0
-#define DEBUGN13 0
-
+#define DEBUG_1 0
+#define DEBUG_2 0
+#define DEBUG_3 0
+#define DEBUG_4 0
+#define DEBUG_5 0
+#define DEBUG_6 1
+#define DEBUG_7 0
+#define DEBUG_8 0
+#define DEBUG_9 0
+#define DEBUG_10 0
+#define DEBUG_11 0
+#define DEBUG_12 0
+#define DEBUG_13 0
 #define ENTREGA_1 0
 #define ENTREGA_2 0
 #define ENTREGA_3 0
-
 // Funciones
 int bmount(const char *camino);
 int bumount();
