2932eb9cc0433d489d42b6ddf4d932a3f16e0e00
PedroFelix8 <pedritofelixj@gmail.com>
2025-05-19 00:22:02 +0200

    mi_rm y mi_rmdir

---
+++ SO_II/mi_rmdir.c
@@ -0,0 +1,45 @@
+#include "directorios.h"
+
+
+int main(int argc, char **argv)
+{
+    char *camino, *ruta;
+    int error;
+    
+    // Comprobar la sintaxis
+    if (argc != 3){
+        fprintf(stderr, RED"Sintaxis: ./mi_rmdir <disco> </ruta_directorio>\n"RESET);
+        return FALLO;
+    }
+
+    // Comprobar que la ruta es un directorio
+    ruta = argv[2];
+    if (ruta[strlen(ruta)-1] != '/'){
+        fprintf(stderr,RED"ERROR: La entrada de '</ruta_directorio>' no es un directorio\n"RESET);
+        return FALLO;
+    }
+
+    camino = argv[1];
+
+    // Montar el sistema de archivos
+    if (bmount(camino) == FALLO){
+        fprintf(stderr, RED"ERROR EN ./mi_rmdir\n"RESET);
+        return FALLO;
+    }
+
+    // Borrar el directorio
+    if ((error = mi_unlink(ruta)) < 0){
+        mostrar_error_buscar_entrada(error);
+        if (error == FALLO){
+            fprintf(stderr, RED"ERROR EN ./mi_rmdir\n"RESET);
+            return FALLO;
+        }
+    }
+
+    // Desmontar el sistema de archivos
+    if (bumount() == FALLO){
+        fprintf(stderr, RED"ERROR EN ./mi_rmdir\n"RESET);
+        return FALLO;
+    }
+    return EXITO;
+}
