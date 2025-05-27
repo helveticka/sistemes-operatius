ff9b1b6e9528e7156283526d77d77541e77b6784
XaviCampos2005 <xavicampos2005@gmail.com>
2025-05-27 21:38:16 +0200

    main de simulacion.c

--- SO_II/simulacion.c
+++ SO_II/simulacion.c
@@ -3,7 +3,70 @@
 int acabados = 0;
 
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Uso: %s <nombre_dispositivo>\n", argv[0]);
+        return -1;
+    }
 
+    // Montar sistema de ficheros
+    if (bmount(argv[1]) == -1) {
+        perror("Error montando el dispositivo");
+        return -1;
+    }
+
+    // Crear directorio /simulacion/
+    if (mi_creat("/simulacion", 6) < 0) {
+        fprintf(stderr, "Error creando el directorio /simulacion\n");
+        bumount();
+        return -1;
+    }
+
+    signal(SIGCHLD, reaper);  // Activamos el reaper
+
+    pid_t pid;
+    for (int i = 0; i < NUMPROCESOS; i++) {
+        pid = fork();
+        if (pid == 0) {  // Hijo
+            char camino[100];
+            struct REGISTRO reg;
+
+            pid_t mi_pid = getpid();
+            sprintf(camino, "/simulacion/%d", mi_pid);
+
+            if (mi_creat(camino, 6) < 0) {
+                fprintf(stderr, "[%d] Error creando el fichero %s\n", mi_pid, camino);
+                exit(1);
+            }
+
+            srand(time(NULL) ^ (getpid()<<16)); // Semilla diferente para cada hijo
+
+            for (int j = 0; j < NUMESCRITURAS; j++) {
+                reg.fecha = time(NULL);
+                reg.pid = mi_pid;
+                reg.nEscritura = j + 1;
+                reg.nRegistro = rand() % REGMAX;
+
+                if (escribir_f(camino, &reg, reg.nRegistro * sizeof(struct REGISTRO), sizeof(struct REGISTRO)) < 0) {
+                    fprintf(stderr, "[%d] Error escribiendo en %s\n", mi_pid, camino);
+                }
+
+                my_sleep(rand() % 51);  // entre 0 y 50 ms
+            }
+
+            exit(0);  // Hijo termina
+        }
+    }
+
+    // Esperamos a que terminen todos
+    while (acabados < NUMPROCESOS) {
+        pause();  // Espera activa hasta recibir SIGCHLD
+    }
+
+    printf("Todos los procesos han terminado (%d)\n", acabados);
+
+    // Desmontar sistema
+    bumount();
+    return 0;
 }
 
 void reaper(){
